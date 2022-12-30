4c2947dce2d5fad4c5634365afe3059465bf3885
iiEthan <ethangibbs2@icloud.com>
2022-12-29 21:46:44 -0600

    Added OpenLayers map

---
+++ main.js
@@ -0,0 +1,20 @@
+import './style.css';
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import {useGeographic} from 'ol/proj.js';
+
+useGeographic();
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: [-96, 37],
+    zoom: 5
+  })
+});
